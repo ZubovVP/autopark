2d0d30bf565767a3d0bf29b84e0aebb1d24c7df9
Zubov <zubov.vp@yandex.ru>
2025-02-10 15:34:22 +0300

    Add key cloak

--- api-gateway/src/main/java/by/javaguru/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/by/javaguru/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package by.javaguru.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
--- api-gateway/src/main/java/by/javaguru/apigateway/config/AppConfig.java
+++ api-gateway/src/main/java/by/javaguru/apigateway/config/AppConfig.java
@@ -1,17 +1,62 @@
 package by.javaguru.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 @Configuration
+@EnableWebFluxSecurity
 public class AppConfig {
+
+    @Value("${spring.cloud.oauth.client.keycloak.issuer-url}")
+    private String ISSUER;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable) // Отключаем CSRF
+                .authorizeExchange(exchanges -> exchanges.pathMatchers("/auth/token").permitAll() // Разрешаем доступ без аутентификации
+                        .pathMatchers("/vehicles/*").hasRole("ADMIN") // Проверяем роль ADMIN
+                        .anyExchange().authenticated() // Все остальные запросы требуют аутентификации
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter())))).build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return ReactiveJwtDecoders.fromIssuerLocation(ISSUER);
+    }
 
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Collection<GrantedAuthority> authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            List<String> roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+            return Stream.concat(authorities.stream(), roles.stream().map(role -> new SimpleGrantedAuthority(role)).map(obj -> GrantedAuthority.class.cast(obj))).toList();
+        });
+        return converter;
     }
 
 }
